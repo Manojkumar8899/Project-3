5423fb64be79f26d48acc4173147c0a3b111eafb
Manojkumar8899 <146886392+Manojkumar8899@users.noreply.github.com>
2023-11-28 16:19:16 -0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+setup(
+    name='Adults_Income_Level_Analysis',
+    version='0.2.1',
+    description='Classes for technical analysis of building dataset.',
+    author='Manoj Kumar Singade',
+    author_email='msingade@mail.yu.edu',
+    license='MIT',
+    url='https://github.com/Manojkumar8899/Project-3',
+    packages=['Adults_Income_Level_Analysis'],
+    install_requires=[
+        'matplotlib>=3.7.1',
+        'numpy>=1.24.3',
+        'pandas>=1.5.3',
+        'seaborn>=0.12.2',
+    ],
+)
